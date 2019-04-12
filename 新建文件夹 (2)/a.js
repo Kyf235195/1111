32087587dca355e3e5e00c0a7f4b848e25c748df
Kyf235195 <43943384+Kyf235195@users.noreply.github.com>
2019-04-12 09:47:02 +0800

    1

---
+++ 新建文件夹 (2)/a.js
@@ -0,0 +1,4 @@
+var pSign=document.getElementById('amao')
+pSign.innerHTML='我就是一段文字'
+pSign.setAttribute('style','color:red ; width:250px ; hight:250px ; background-cp;pr:blue; position: relative;top:250px ; left:250px')
+window.anload
